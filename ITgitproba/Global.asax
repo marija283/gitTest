90a6cb3efc6a7e0a39e1b9d0ff670f4cb153f19c
Marija <marija283@hotmail.com>
2015-08-17 20:36:11 +0200

    first commit

---
+++ ITgitproba/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITgitproba.Global" Language="C#" %>
---
+++ ITgitproba/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITgitproba.Startup))]
+namespace ITgitproba
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
